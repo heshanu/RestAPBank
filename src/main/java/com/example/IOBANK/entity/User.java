0eb5f94bc506516e7ebe7bf0c69b89225d50c568
heshanu <heshanu97@gmail.com>
2024-10-01 22:50:12 +0530

    completed entity

--- src/main/java/com/example/IOBANK/entity/User.java
+++ src/main/java/com/example/IOBANK/entity/User.java
@@ -23,89 +23,38 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @Table(name = "bank_user")
 public class User implements UserDetails {
-    /**
-     * Unique identifier for the user.
-     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String uid;
-
-    /**
-     * First name of the user.
-     */
     private String firstname;
-
-    /**
-     * Last name of the user.
-     */
     private String lastname;
-
-    /**
-     * Unique username for the user.
-     */
     @Column(nullable = false, unique = true)
     private String username;
-
-    /**
-     * Date of birth of the user.
-     */
     private Date dob;
-
-    /**
-     * Telephone number of the user.
-     */
     private long tel;
-
-    /**
-     * Additional tag for the user.
-     */
     private String tag;
 
-    /**
-     * Encrypted password for the user.
-     */
     @JsonIgnore
     private String password;
 
-    /**
-     * Gender of the user.
-     */
     private String gender;
 
-    /**
-     * Timestamp of when the user was created.
-     */
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    /**
-     * Timestamp of when the user was last updated.
-     */
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-    /**
-     * List of roles assigned to the user.
-     */
     private List<String> roles;
 
-    /**
-     * One-to-one relationship with the Card entity, representing the user's card.
-     */
     @OneToOne(mappedBy = "owner")
     @JsonIgnore
     private Card card;
 
-    /**
-     * One-to-many relationship with the Transaction entity, representing the user's transactions.
-     */
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Transaction> transactions;
 
-    /**
-     * One-to-many relationship with the Account entity, representing the user's accounts.
-     */
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Account> accounts;
