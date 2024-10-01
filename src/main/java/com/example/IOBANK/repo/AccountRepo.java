96715aa0e007495c858b9a662ae52e2e44f14730
heshanu <heshanu97@gmail.com>
2024-10-01 23:03:54 +0530

    completed

---
+++ src/main/java/com/example/IOBANK/repo/AccountRepo.java
@@ -0,0 +1,15 @@
+package com.example.IOBANK.repo;
+
+import com.example.IOBANK.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepo extends JpaRepository<String, Account> {
+    boolean existsByAccountNumber(Long accountNumber);
+    boolean existsByCodeAndOwnerUid(String code, String uid);
+    List<Account> findAllByOwnerUid(String uid);
+    Optional<Account> findByCodeAndOwnerUid(String code, String uid);
+    Optional<Account> findByAccountNumber(long recipientAccountNumber);
+}
